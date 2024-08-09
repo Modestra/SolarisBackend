8b30244abb2f05c50c5f3f208a235de7435cc8e1
Modestra <umbrellamixmail@gmail.com>
2024-08-09 23:29:15 +0700

    fix

--- solaris/solaris/asgi.py
+++ solaris/solaris/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for bushtree project.
+ASGI config for solaris project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bushtree.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'solaris.settings')
 
 application = get_asgi_application()
