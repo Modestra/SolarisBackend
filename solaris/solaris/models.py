8ae317c61ac78bbe9c00c802687bf80dac3affca
Modestra <umbrellamixmail@gmail.com>
2024-08-13 01:37:01 +0700

    Добавление проекта магазина

--- solaris/solaris/models.py
+++ solaris/solaris/models.py
@@ -14,7 +14,8 @@ class Users(models.Model):
     user_id = models.UUIDField(default=uuid.uuid4)
     login = models.TextField()
     password = models.TextField()
-    category = models.ForeignKey(Category, to_field="category_id", on_delete=models.SET_DEFAULT, default=2) 
+    category = models.ForeignKey(Category, to_field="category", on_delete=models.SET_DEFAULT, default="Ученик", db_constraint=True)
+    date_seccion = models.CharField()
     
     def __str__(self):
         return self.name
